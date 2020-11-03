115d3c55cb709a734cc466939f027b47507ea331
Miftahul Huda <kodeingusan@gmail.com>
2020-11-03 15:33:09 +0700

    -

--- application/controllers/Barang.php
+++ application/controllers/Barang.php
@@ -7,7 +7,7 @@ class Barang extends CI_Controller
     function __construct()
     {
         parent::__construct();
-        $this->load->model('Main_model', 'main');
+        $this->load->model('main');
         $this->load->library('session');
     }
     public function index()
